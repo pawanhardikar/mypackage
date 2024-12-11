54d06274a694ecd83c102640b1341f158bc266be
pawanhardikar <pawanhardikar@gmail.com>
2024-12-11 13:46:02 +0530

    Changed test_mypackage file to import properly

--- tests/test_mypackage.py
+++ tests/test_mypackage.py
@@ -1,3 +1,6 @@
+import sys 
+import os 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from mypackage import add, subtract 
 def test_add(): 
     assert add(1, 2) == 3 
